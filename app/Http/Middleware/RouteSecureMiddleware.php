3519f68b920f450e36e7c48f1c52b6e4ee009a3a
andrea.damiani <andrea.damiani@r2u.it>
2025-09-19 16:22:45 +0200

    test

---
+++ app/Http/Middleware/RouteSecureMiddleware.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Config;
+
+class RouteSecureMiddleware
+{
+    /**
+   
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $apiKey = env('API_KEY');
+        
+       
+        if ($request->header('X-API-KEY') !== $apiKey) {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+
+        return true;
+    }
+}
